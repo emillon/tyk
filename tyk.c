03b037d9247c14443681a647bcaaef4a1ce4c207
Etienne Millon <etienne.millon@gmail.com>
2012-05-31 12:59:10 +0200

    execvp tyk arguments

--- tyk.c
+++ tyk.c
@@ -1,4 +1,26 @@
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <sysexits.h>
+#include <unistd.h>
+
+static void usage(char * progname)
 {
+    printf("Usage : %s prog args\n", progname);
+    exit(EX_USAGE);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        usage(argv[0]);
+    }
+
+    char * progname = argv[1];
+
+    int z = execvp(progname, &argv[1]);
+    if (z != 0) {
+        perror("execvp");
+    }
+
     return 0;
 }
