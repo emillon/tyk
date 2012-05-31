1a3a2144e9f04ef40248ad6bcfdbb92c948e2169
Etienne Millon <etienne.millon@gmail.com>
2012-05-31 13:30:11 +0200

    Print time elapsed after command execution

--- tyk.c
+++ tyk.c
@@ -1,14 +1,46 @@
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sysexits.h>
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
+
+static struct timeval time_start;
+
+static void print_timedelta(time_t *delta)
+{
+    struct tm *t = localtime(delta);
+
+    /* Normalize because delta is not a date */
+    t->tm_year -= 70;
+    t->tm_mday -= 1;
+    t->tm_hour -= 1;
+
+    printf("\n");
+    if(t->tm_year >0) printf("%dy ", t->tm_year);
+    if(t->tm_mon  >0) printf("%dm ", t->tm_mon);
+    if(t->tm_mday >0) printf("%dd ", t->tm_mday);
+    if(t->tm_hour >0) printf("%dh ", t->tm_hour);
+    if(t->tm_min  >0) printf("%dm ", t->tm_min);
+    if(t->tm_sec  >0) printf("%ds" , t->tm_sec);
+    printf("\n");
+}
 
 static void handle_sigchld(int signum)
 {
     if (signum == SIGCHLD) {
+        struct timeval time_end, delta;
+
+        int z = gettimeofday(&time_end, NULL);
+        if (z != 0) {
+            perror("gettimeofday");
+        }
+
+        timersub (&time_end, &time_start, &delta);
+        print_timedelta(&delta.tv_sec);
         exit(0);
     }
 }
@@ -26,6 +58,11 @@ int main(int argc, char** argv)
     }
 
     char * progname = argv[1];
+
+    int z = gettimeofday(&time_start, NULL);
+    if (z != 0) {
+        perror("gettimeofday");
+    }
 
     pid_t pid = fork();
 
